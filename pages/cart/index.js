823fc0a61edea1dcc2c12079c93bc686260435c9
Mahbub Hasan <99654070+mahbubhswe@users.noreply.github.com>
2022-04-02 11:23:28 +0600

    Update index.js

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -12,7 +12,7 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { contextStore } from "../../utils/Store";
-import Protected from "../../Components/Protected";
+import Protected from "../../components/Protected";
 import { DeleteIcon, MinusIcon, AddIcon } from "@chakra-ui/icons";
 import Image from "next/image";
 import { useRouter } from "next/router";
